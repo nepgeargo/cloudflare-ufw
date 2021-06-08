a25cbe7135234c552fd379fec75bfba579539364
nepgeargo <8287084+nepgeargo@users.noreply.github.com>
2021-06-08 16:36:56 +1000

    Update cloudflare-ufw.sh

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -10,9 +10,9 @@ curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cf_ips
 #for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 80 comment 'Cloudflare'; done
 
 # Restrict to port 443
-#for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 443 comment 'Cloudflare'; done
+for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 443 comment 'Cloudflare'; done
 
 # Restrict to ports 80 & 443
-for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare'; done
+#for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare'; done
 
 ufw reload > /dev/null
